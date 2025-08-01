d7c1e0aacf2d85cdd09204171f8f6273782521c8
ademiando <ademiandoo@gmail.com>
2025-08-01 10:20:20 +0800

    Update animation.js

--- 1/animation.js
+++ 1/animation.js
@@ -1,6 +1,30 @@
+// animation.js
+
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.utils.toArray(".stage").forEach((section, i) => { const media = section.querySelector("video, img");
+// Apply animation to each section
+gsap.utils.toArray(".stage").forEach((section) => {
+  const media = section.querySelector("video, img");
 
-gsap.fromTo(media, { scale: 2.5, opacity: 0, filter: "blur(20px)" }, { scale: 1, opacity: 1, filter: "blur(0px)", ease: "power2.out", scrollTrigger: { trigger: section, start: "top center", end: "bottom center", scrub: true, pin: true, anticipatePin: 1 } } ); });
-
+  gsap.fromTo(media,
+    {
+      scale: 2.5,
+      opacity: 0,
+      filter: "blur(20px)"
+    },
+    {
+      scale: 1,
+      opacity: 1,
+      filter: "blur(0px)",
+      ease: "power2.out",
+      scrollTrigger: {
+        trigger: section,
+        start: "top center",
+        end: "bottom center",
+        scrub: true,
+        pin: true,
+        anticipatePin: 1
+      }
+    }
+  );
+});
